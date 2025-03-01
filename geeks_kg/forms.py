416842a3b79179bdb9497b3d63912ae9441e9521
МАрдон <creatermari@gmail.com>
2025-03-01 15:04:11 +0600

    Добавил контакты

--- geeks_kg/forms.py
+++ geeks_kg/forms.py
@@ -5,4 +5,4 @@ from .models import Cours, ContactForm
 class ContactFormForm(forms.ModelForm):
     class Meta:
         model = ContactForm  
-        fields = ['first_name', 'last_name', 'age', 'phone_number'] 
+        fields = ['first_name', 'last_name', 'age', 'phone_number']
